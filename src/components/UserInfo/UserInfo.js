3f42fc93d0560c3acea18ad1d514346b33c95c96
Jeffrey Lin <jeffreylin0312@gmail.com>
2022-11-13 14:57:41 +1000

    feat: static user tag

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -20,7 +20,6 @@ const Description = styled.div`
 const Container = styled.div`
   display: flex;
   align-items: flex-end;
-  /* justify-content: space-around; */
   font-size: 1.5rem;
   margin-left: 0;
 `;
