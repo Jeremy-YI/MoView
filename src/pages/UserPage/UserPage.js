1328e95f4ba2754b52c73c4b91cc2b66ada17bdc
Jeffrey Lin <jeffreylin0312@gmail.com>
2022-11-13 14:57:41 +1000

    feat: static user tag

--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -2,14 +2,20 @@ import React from 'react';
 import Grid from '@mui/material/Grid';
 import MuiBox from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
-import Avatar from '@mui/material/Avatar';
+import MuiAvatar from '@mui/material/Avatar';
 import UserInfo from '../../components/UserInfo/UserInfo';
 import UserTag from '../../components/UserTag/UserTag';
 import Root from '../../layout/Root';
 
+const Avatar = styled(MuiAvatar)({
+  marginTop: '2em',
+  marginLeft: '2em',
+});
+
 const Box = styled(MuiBox)({
   paddingTop: '4em',
   paddingLeft: '5em',
+  marginBottom: '5em',
 });
 
 function UserPage() {
@@ -21,7 +27,7 @@ function UserPage() {
           <Grid item xs={2}>
             <Avatar
               alt="Remy Sharp"
-              src="/static/images/avatar/1.jpg"
+              src="https://www.denofgeek.com/wp-content/uploads/2019/02/mcu-1-iron-man.jpg?fit=1200%2C675"
               sx={{ width: 150, height: 150 }}
             />
           </Grid>
