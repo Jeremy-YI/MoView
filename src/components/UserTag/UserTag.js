f009a4edb17c76d770aebadd239d018a07c4101b
Jeffrey Lin <jeffreylin0312@gmail.com>
2022-11-17 12:49:44 +1000

    feat: read tags and fix error

--- src/components/UserTag/UserTag.js
+++ src/components/UserTag/UserTag.js
@@ -7,13 +7,13 @@ import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import useEffect from 'react';
+import { useEffect } from 'react';
 import { fetchAllTags, selectTag } from '../../redux/slices/tag';
 
 const style = {
   width: '100%',
   maxWidth: 360,
-  height: 250,
+  height: 300,
   bgcolor: '#e3e3e3',
   borderStyle: 'solid',
   fontSize: '1.5rem',
@@ -22,21 +22,21 @@ const style = {
 
 export default function UserTag() {
   const dispatch = useDispatch();
-  const tagList = useSelector(selectTag);
+  const tagList = useSelector((state) => state.tag.tagList);
   useEffect(() => {
     dispatch(fetchAllTags());
   }, []);
 
   return (
-    <>
+    <List container sx={style} component="nav" aria-label="mailbox folders">
       {tagList.map((tag) => (
-        <List sx={style} component="nav" aria-label="mailbox folders">
-          <ListItem key={tag.id}>
+        <>
+          <ListItem item key={tag.id}>
             <ListItemText primary={tag.name} />
           </ListItem>
           <Divider />
-        </List>
+        </>
       ))}
-    </>
+    </List>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,7 @@ import movieReducer from './slices/movie';
 import reviewReducer from './slices/review';
 import commentReducer from './slices/comment';
 import userReducer from './slices/user';
+import tagReducer from './slices/tag';
 
 const reducer = combineReducers({
   count: counterReducer,
@@ -11,6 +12,7 @@ const reducer = combineReducers({
   comment: commentReducer,
   review: reviewReducer,
   user: userReducer,
+  tag: tagReducer,
 });
 
 const store = configureStore({
