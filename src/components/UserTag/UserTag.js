a5664ac9286ba980e1ab89564194fdb0b29fe9fd
Jeffrey Lin <jeffreylin0312@gmail.com>
2022-11-17 12:49:44 +1000

    feat: read tags and fix error

---
+++ src/components/UserTag/UserTag.js
@@ -0,0 +1,42 @@
+/* eslint-disable import/named */
+/* eslint-disable no-unused-vars */
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchAllTags, selectTag } from '../../redux/slices/tag';
+
+const style = {
+  width: '100%',
+  maxWidth: 360,
+  height: 300,
+  bgcolor: '#e3e3e3',
+  borderStyle: 'solid',
+  fontSize: '1.5rem',
+  justifyContent: 'center',
+};
+
+export default function UserTag() {
+  const dispatch = useDispatch();
+  const tagList = useSelector((state) => state.tag.tagList);
+  useEffect(() => {
+    dispatch(fetchAllTags());
+  }, []);
+
+  return (
+    <List container sx={style} component="nav" aria-label="mailbox folders">
+      {tagList.map((tag) => (
+        <>
+          <ListItem item key={tag.id}>
+            <ListItemText primary={tag.name} />
+          </ListItem>
+          <Divider />
+        </>
+      ))}
+    </List>
+  );
+}
---
+++ src/pages/UserPage/UserPage.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import MuiBox from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+import MuiAvatar from '@mui/material/Avatar';
+import UserInfo from '../../components/UserInfo/UserInfo';
+import UserTag from '../../components/UserTag/UserTag';
+import Root from '../../layout/Root';
+import PopularReview from '../../components/PopularReview/PopularReview';
+import UserPageButtons from '../../components/UserPageButtons/UserPageButtons';
+
+const Avatar = styled(MuiAvatar)({
+  marginTop: '3em',
+  marginLeft: '2em',
+});
+
+const Box = styled(MuiBox)({
+  paddingTop: '4em',
+  paddingLeft: '5em',
+  marginBottom: '5em',
+});
+
+function UserPage() {
+  return (
+    <>
+      <Root />
+      <Box sx={{ flexGrow: 3 }}>
+        <Grid container spacing={3}>
+          <Grid item xs={2}>
+            <Avatar
+              alt="Remy Sharp"
+              src="https://www.denofgeek.com/wp-content/uploads/2019/02/mcu-1-iron-man.jpg?fit=1200%2C675"
+              sx={{ width: 170, height: 170 }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <UserInfo />
+          </Grid>
+          <Grid item xs={3}>
+            <UserTag />
+          </Grid>
+        </Grid>
+      </Box>
+      <UserPageButtons />
+      <PopularReview />
+    </>
+  );
+}
+
+export default UserPage;
