85cf3668b52f2f7afc78d0faf1ab4a5cdccf2424
Zora <zora.wangyanzi@gmail.com>
2022-12-01 12:56:58 +0000

    Feature/MOVIEW-124 fe footer update  (pull request #61)
    update footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,16 +6,37 @@ import Box from '@mui/material/Box';
 const FooterWrapper = styled('footer')({ marginTop: '40px' });
 
 const Container = styled(Box)`
-  margin-top: 60px;
+  margin-block: 40px;
   width: 100%;
+  text-align: center;
+  color: grey;
+`;
+const ItemsContainer = styled(Box)`
+  display: flex;
+  justify-content: center;
+  gap: 3%;
+`;
+const FooterDivider = styled(Divider)`
+  margin-block: 10px;
+  margin-inline: 5%;
+  text-align: center;
 `;
 
 export default function Footer() {
   return (
     <FooterWrapper>
       <Container>
-        <Divider />
-        MoView 2022
+        <ItemsContainer>
+          <span>Help</span>
+          <span>Privacy Policy</span>
+          <span>Term of Use</span>
+          <span>Legal</span>
+          <span>About Us</span>
+          <span>Contact Us</span>
+          <span>Join Us</span>
+        </ItemsContainer>
+        <FooterDivider />
+        <div>Copyright ©2022 MoView Team: YourView &nbsp; &nbsp;All rights reserved.</div>
       </Container>
     </FooterWrapper>
   );
